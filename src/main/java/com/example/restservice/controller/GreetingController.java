8300b2370699775f5a7571a99086963593612a98
khoanxl918@gmail.com <khoanxl918@gmail.com>
2020-05-24 23:20:16 +0700

    Adding Greeting resource presentation and REST controller.

---
+++ src/main/java/com/example/restservice/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.restservice.controller;
+
+import com.example.restservice.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * for something like: http://localhost:8080/greeting?name=Khoa
+     * @param name
+     * @return
+     */
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
